97945c5053f7b72b14d2e806d825069907814592
Steven Nguyen <steven.nguyenhung@gmail.com>
2020-04-15 01:44:34 +0800

    Themify the app

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,28 @@ import { jsx } from "@emotion/core";
 import Header from "./components/Header";
 import MainContainer from "./components/MainContainer";
 import Footer from "./components/Footer";
+import { ThemeProvider } from 'emotion-theming';
+
+const theme = {
+  color: {
+    darkGreen: "#356223",
+    lightGreen: "#4d960a",
+    lightGrey: "#877f87",
+    darkGrey: "#eccc70",
+    lightYellow: "#f8daa2",
+    darkYellow: "#949012",
+    lightOrange: "#deab1c",
+    darkOrange: "#ffe94d"
+  }
+}
 
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <Header />
       <MainContainer />
       <Footer />
-    </div>
+    </ThemeProvider>
   );
 }
 
